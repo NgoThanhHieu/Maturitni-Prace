fe024c5b6e1ebad7a7138328494319c4b404d971
NgoThanhHieu <Thanhhieu.ngo@student.ossp.cz>
2022-09-06 10:21:53 +0200

    1

---
+++ app/Router/RouterFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Router;
+
+use Nette;
+use Nette\Application\Routers\RouteList;
+
+
+final class RouterFactory
+{
+	use Nette\StaticClass;
+
+	public static function createRouter(): RouteList
+    {
+        $router = new RouteList;
+        $router->addRoute('<module>/<presenter>/<action>[/<id \d+>]', [
+            'module' => 'Admin',
+            'presenter' => 'Dashboard',
+            'action' => 'default',
+        ]);
+        return $router;
+    }
+}
